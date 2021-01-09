8f96adddde3487ee1fa0a71569cb638c144d1cc5
moroya <shoki0311@gmail.com>
2021-01-09 15:53:28 +0900

    Refactoring

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,133 +10,192 @@
 </template>
 
 <script>
-// @ is an alias to /src
 import { Loader } from "@googlemaps/js-api-loader";
 
-let map;
+let google, map, place;
 
 export default {
-  name: "Home",
+  name: "Map",
 
   data() {
     return {
-      apiKey: process.env.VUE_APP_GOOGLE_MAP_API_KEY,
+      apiKey: process.env.VUE_APP_GOOGLE_MAP_API_KEY, // API KEY（MapとPlaceのみ有効にしてあるやつ）
+      geoPositioning: false, // 現在地を取得中かどうか
+
+      // Google Mapの設定
       mapConfig: {
-        mapId: "37351173ff5408f5",
+        mapId: "37351173ff5408f5", // あらかじめコンソールで作ってある地図スタイルを指定
+        gestureHandling: "greedy", // スマホで2本指でズームとか出さないようにする
+
+        // とりあえず東京駅
         center: {
           lat: 35.6812362,
           lng: 139.7671248
         },
         zoom: 16,
+
+        // ズーム可能範囲
         minZoom: 9,
-        maxZoom: 19,
-        gestureHandling: "greedy"
+        maxZoom: 19
       }
     };
   },
 
-  methods: {
-    panToCurrentLocation() {
-      // Try HTML5 geolocation.
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(
-          position => {
-            const pos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-            };
+  // パラメータ初期化
+  created() {
+    // localStorageに最後に表示した位置情報があれば上書き
+    const lat = localStorage.getItem("lat");
+    const lng = localStorage.getItem("lng");
 
-            localStorage.setItem("lastLat", pos.lat);
-            localStorage.setItem("lastLng", pos.lng);
-
-            map.setCenter(pos);
-            map.setZoom(16);
-          },
-          () => {
-            this.handleLocationError(true, map.getCenter());
-          }
-        );
-      } else {
-        // Browser doesn't support Geolocation
-        this.handleLocationError(false, map.getCenter());
-      }
-    },
-
-    handleLocationError(browserHasGeolocation, pos) {
-      console.log(pos);
-      // infoWindow.setPosition(pos);
-      // infoWindow.setContent(
-      //   browserHasGeolocation
-      //     ? "Error: The Geolocation service failed."
-      //     : "Error: Your browser doesn't support geolocation."
-      // );
+    if (lat && lng) {
+      this.mapConfig.center.lat = Number(lat || this.mapConfig.center.lat);
+      this.mapConfig.center.lng = Number(lng || this.mapConfig.center.lng);
     }
   },
 
-  created() {
-    this.mapConfig.center.lat = Number(
-      localStorage.getItem("lastLat") || this.mapConfig.center.lat
-    );
-    this.mapConfig.center.lng = Number(
-      localStorage.getItem("lastLng") || this.mapConfig.center.lng
-    );
-  },
-
+  // DOM初期化
   async mounted() {
-    await new Loader({
-      apiKey: this.apiKey,
-      version: "weekly",
-      mapIds: [this.mapConfig.mapId],
-      libraries: ["places"]
-    }).load();
+    // Google Map SDKをロードする
+    if (!window.google) {
+      await new Loader({
+        apiKey: this.apiKey,
+        version: "weekly",
+        mapIds: [this.mapConfig.mapId],
+        libraries: ["places"] // Place APIを有効化
+      }).load();
+    }
 
-    const google = window.google;
+    // Google Mapを初期化
+    google = window.google;
     map = new google.maps.Map(this.$refs.map, this.mapConfig);
 
-    // infoWindow = new google.maps.InfoWindow({
-    //   content: ""
-    // });
+    // Place APIを初期化
+    place = new google.maps.places.PlacesService(map);
 
-    google.maps.event.addListenerOnce(map, "tilesloaded", () => {
-      window.SuperClusterAdapterLoader.getClusterer().then(Clusterer => {
-        if (Clusterer) {
-          const clusterer = new Clusterer.Builder(map)
-            .withRadius(250)
-            .withMaxZoom(19)
-            .withCustomMarkerIcon(
-              () =>
-                "https://jawj.github.io/OverlappingMarkerSpiderfier/marker.svg"
-            )
-            .withMarkerClick((marker, event) => {
-              // infoWindow.close();
-              map.panTo(event.latLng);
+    // 初回のみ、タイル読み込みが完了したらGeoJSONをロード
+    google.maps.event.addListenerOnce(map, "tilesloaded", this.loadGeoJson);
+  },
 
-              // const content = `<h2>${marker.title}</h2><h3>${marker.placeId}</h3>`;
-              // infoWindow.setContent(content);
-              // infoWindow.open(map, marker);
-              
-              // map.panTo(event.latLng);
-              console.log(event.latLng);
+  methods: {
+    /*
+     * GeoJSONをロードしてクラスタリング
+     */
+    async loadGeoJson() {
+      // Superclusterを初期化
+      const Clusterer = await window.SuperClusterAdapterLoader.getClusterer();
 
-            })
-            .build();
+      if (!Clusterer) return;
 
-          fetch(
-            "tokyo_pdf2.geojson"
-            // "https://raw.githubusercontent.com/moroya/goto-eta/main/public/all.geojson"
-          )
-            .then(response => {
-              return response.json();
-            })
-            .then(data => {
-              clusterer.load(data);
-            })
-            .catch(err => {
-              console.log("Cannot fetch GeoJSON data for this example", err);
-            });
+      // クラスタを初期化
+      const clusterer = new Clusterer.Builder(map)
+        .withRadius(250)
+        .withMaxZoom(19)
+        .withCustomMarkerIcon(this.markerIconByGenre)
+        .withMarkerClick(this.markerClickHandler)
+        .build();
+
+      // GeoJSONをロード
+      try {
+        const geojson = await fetch("tokyo_pdf2.geojson").then(res =>
+          res.json()
+        );
+        clusterer.load(geojson);
+      } catch (err) {
+        console.log("Cannot fetch GeoJSON data for this example", err);
+      }
+    },
+
+    /*
+     * マーカーがクリックされたとき
+     */
+    markerClickHandler(marker, event) {
+      map.panTo(event.latLng);
+
+      place.getDetails(
+        {
+          placeId: marker.placeId,
+          fields: [
+            "formatted_address",
+            "name",
+            "photo",
+            "url",
+            "vicinity",
+            "formatted_phone_number",
+            "website",
+            "rating",
+            "reviews",
+            "user_ratings_total"
+          ]
+        },
+        (place, status) => {
+          if (status == google.maps.places.PlacesServiceStatus.OK) {
+            console.log(place);
+          }
         }
-      });
-    });
+      );
+    },
+
+    /*
+     * ジャンルからマーカーのアイコンを設定
+     */
+    markerIconByGenre() {
+      // console.log(marker.properties.genre);
+      return "https://jawj.github.io/OverlappingMarkerSpiderfier/marker.svg";
+    },
+
+    /*
+     * 現在位置を取得して地図をパン
+     */
+    panToCurrentLocation() {
+      // Geolocationをサポートしていないブラウザがあるのかよ。たまにあるんだよなぁ
+      if (!navigator.geolocation) {
+        this.handleLocationError();
+      }
+
+      // 現在地を取得開始（UIに反映）
+      this.geoPositioning = true;
+
+      // 現在位置を取得
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          const pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          };
+
+          // 位置をlocalStorageに保存しておく
+          // TODO: 最後に表示した位置のほうがいいと思う
+          localStorage.setItem("lat", pos.lat);
+          localStorage.setItem("lng", pos.lng);
+
+          // 地図をパン
+          map.setZoom(16);
+          map.panTo(pos);
+          this.geoPositioning = false;
+        },
+        // エラーハンドリング
+        this.handleLocationError
+      );
+    },
+
+    /*
+     * 現在位置の取得エラーハンドリング
+     */
+    handleLocationError(err) {
+      if (!err) {
+        console.log("Geolocation is not supported for this Browser/OS");
+      } else if (err.code === 0) {
+        console.log("Unknown error");
+      } else if (err.code === 1) {
+        console.log("Permission denied");
+      } else if (err.code === 2) {
+        console.log("Position unavailable");
+      } else if (err.code === 3) {
+        console.log("Timed out");
+      }
+
+      this.geoPositioning = false;
+    }
   }
 };
 </script>
